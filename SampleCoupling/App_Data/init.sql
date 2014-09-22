9e7513a446fc330ff08152d3b7ab4534b7399e68
Roberto Luis Bisbe <rober90@gmail.com>
2014-09-22 22:02:56 +0200

    First aproach, no decoupling

---
+++ SampleCoupling/App_Data/init.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(50) NULL, 
+    [Author] NVARCHAR(50) NULL
+)
+INSERT INTO [dbo].[Table] (Title,Author) VALUES ('Hamlet', 'William Shakespeare')
+INSERT INTO [dbo].[Table] (Title,Author) VALUES ('El Ingenioso Hidalgo Don Quijote de la Mancha', 'Miguel de Cervantes y Saavedra')
+INSERT INTO [dbo].[Table] (Title,Author) VALUES ('La divina comedia', 'Dante Alighieri')
---
+++ SampleCoupling/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using SampleCoupling.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleCoupling.Controllers
+{
+public class HomeController : Controller
+{
+    public ActionResult Books()
+    {
+        List<Book> books = new List<Book>();
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDb)\v11.0;AttachDbFilename=|DataDirectory|\Books.mdf;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+
+        cmd.CommandText = "SELECT Title,Author FROM [Table]";
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = connection;
+
+        connection.Open();
+
+        reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                books.Add(new Book
+                {
+                    Title = reader.GetString(0),
+                    Author = reader.GetString(1)
+                });
+            }
+        }
+
+        connection.Close();
+        return View(books);
+    }
+}
+}
